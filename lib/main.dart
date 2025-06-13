6918ab4af3d376974974612a363c7bea88251ff9
EldhoRT-Dev <eldhoroy.dev@gmail.com>
2025-06-13 15:06:05 +0530

    Initial commit with folders

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:interview_task/feature/auth/view/login_page.dart';
+
+void main() {
+  runApp(ShoppingCart());
+}
+
+class ShoppingCart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
