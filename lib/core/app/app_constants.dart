e1cf35c098fb067fa117be9ecb8e5d869ea34cc8
EldhoRT-Dev <97051181+Eldho1234@users.noreply.github.com>
2025-06-14 19:28:33 +0530

    update admi login credentials

--- lib/core/app/app_constants.dart
+++ lib/core/app/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
   static const String baseUrl = 'https://dummyjson.com/products';
-  static const String adminEmail = 'admin@example.com';
+  static const String adminEmail = 'admin@gmail.com';
   static const String adminPassword = 'admin123';
 }
